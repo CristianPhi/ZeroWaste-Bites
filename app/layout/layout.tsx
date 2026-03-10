0b9f1255fdddfd09e21abaf556cc83068c0dabd5
Cristian <cristian.philander06@gmail.com>
2026-03-10 11:21:19 +0700

    update

--- app/layout/layout.tsx
+++ app/layout/layout.tsx
@@ -1,5 +1,26 @@
-export const metadata = 
-{
-  title: 'ZeroWaste Bites - Solusi Makanan Berlebih',
-  description: 'Aplikasi penyelamat makanan untuk mengurangi limbah makanan di restoran.',
+export const metadata = {
+  title: 'ZeroWaste Bites - Penyelamat Makanan Berlebih',
+  description: 'Bantu kurangi limbah makanan dan selamatkan makanan lezat dari restoran favoritmu dengan harga terjangkau.',
+  openGraph: {
+    title: 'ZeroWaste Bites - Solusi Limbah Makanan',
+    description: 'Selamatkan makanan, kurangi limbah. Bergabunglah dengan gerakan ZeroWaste Bites!',
+    url: 'https://zerowastebites-rust.vercel.app',
+    siteName: 'ZeroWaste Bites',
+    images: [
+      {
+        url: 'https://zerowastebites-rust.vercel.app/images/Logo.png', // Pastikan logo kamu ada di folder ini
+        width: 1200,
+        height: 630,
+        alt: 'ZeroWaste Bites Preview',
+      },
+    ],
+    locale: 'id_ID',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ZeroWaste Bites',
+    description: 'Solusi cerdas kurangi food waste.',
+    images: ['https://zerowastebites-rust.vercel.app/images/Logo.png'],
+  },
 }
